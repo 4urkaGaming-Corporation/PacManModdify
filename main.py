d3c3a3113f4f4c320cb62672bdb21d2aa921243b
Maxim Starostyn <maximstarostyn@gmail.com>
2025-02-25 20:51:57 +0200

    +half menu

--- main.py
+++ main.py
@@ -16,6 +16,47 @@ pygame.display.set_caption("Pac Man: FunModify")
 # Шрифт для тексту
 font = pygame.font.Font(None, 36)
 
+
+
+# Функція для малювання кнопок
+def draw_button(text, x, y, width, height, color, text_color):
+    pygame.draw.rect(window, color, (x, y, width, height))
+    label = font.render(text, True, text_color)
+    window.blit(label, (x + (width - label.get_width()) // 2, y + (height - label.get_height()) // 2))
+
+
+# Функція головного меню
+def main_menu():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                # Перевірка натискання на кнопку "Start"
+                if 300 <= mouse_x <= 500 and 200 <= mouse_y <= 250:
+                    game = Game()
+                    game.run()
+                    return
+                # Перевірка натискання на кнопку "Info"
+                # тут буде перевірка натискання на кнопку Інфо
+
+
+
+        # Малювання меню
+        window.fill((0, 0, 0))  # Заповнення екрану чорним
+        draw_button("Start", 300, 200, 200, 50, (0, 255, 0), (255, 255, 255))
+        draw_button("Info", 300, 300, 200, 50, (0, 0, 255), (255, 255, 255))
+        
+        pygame.display.flip()
+
+# Функція для відображення інформації
+# def show_info():
+   
+
+
+
 # Запуск гри
 if name == "main":
     game = Game()
