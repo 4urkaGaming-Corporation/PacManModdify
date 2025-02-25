0eca1efde12357e755ea6905328e4091a846147a
Maxim Starostyn <maximstarostyn@gmail.com>
2025-02-25 19:25:06 +0200

    starting menu window

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
+import pygame_gui
 # from game import Game - в майбутньому реалізуємо файл game для логіки гри та основного циклу
 
 # Инициализация Pygame
 pygame.init()
+
+
+# Розміри вікна
+WIDTH = 800
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pac Man: FunModify")
+
 
 # Запуск гри
 if name == "main":
     game = Game()
     game.run()
     pygame.quit()
+
+
