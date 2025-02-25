a535994edb815bfb9b1a98fa200bb28fd9e102cc
Maxim Starostyn <maximstarostyn@gmail.com>
2025-02-25 19:34:26 +0200

    add font

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pac Man: FunModify")
 
 
+# Шрифт для тексту
+font = pygame.font.Font(None, 36)
+
 # Запуск гри
 if name == "main":
     game = Game()
