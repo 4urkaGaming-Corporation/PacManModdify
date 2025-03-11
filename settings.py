b569523827b269c8bf8eab5fdfb5abc075240e2e
nwpivpe <sanyaaaap@gmail.com>
2025-03-11 15:10:28 +0200

    EnemyParameters+

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Цвета
+# Кольори
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 BLUE = (0, 0, 255)
@@ -7,7 +7,11 @@ RED = (255, 0, 0)
 PINK = (255, 105, 180)
 CYAN = (0, 255, 255)
 ORANGE = (255, 165, 0)
-# Параметры Пекмена
+# Параметри Пекмена
 PACMAN_RADIUS = 15
 PACMAN_SPEED = 5
-INITIAL_PACMAN_POS = [WIDTH // 2 - 50, HEIGHT // 2]
+INITIAL_PACMAN_POS = [800 // 2 - 50, 600 // 2]
+# Параметри ворогів
+ENEMY_RADIUS = 15
+ENEMY_SPEED = 3
+VISIBILITY_RANGE = 200
