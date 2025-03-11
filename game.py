94b6cd2e0ca74b566a2d68e0600a56487c25eedc
DanielKNU <naumenkodaniel003@knu.ua>
2025-03-11 18:30:34 +0200

    Adding game entities

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 import pygame
 from settings import *
+from entities import Pacman, Enemy
+from maze import Maze
 
 class Game:
     def init(self):
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Пекмен с умными призраками")
         self.clock = pygame.time.Clock()
+        self.pacman = Pacman()
+        self.enemies = [Enemy(enemy["pos"], enemy["color"]) for enemy in INITIAL_ENEMIES]
+        self.maze = Maze()
         self.score = 0
         self.font = pygame.font.Font(None, 36)
         self.game_over = False
@@ -18,6 +23,11 @@ class Game:
                     running = False
 
             self.window.fill(BLACK)
+            self.maze.draw(self.window)
+            self.pacman.draw(self.window)
+            for enemy in self.enemies:
+                enemy.draw(self.window)
+
             score_text = self.font.render(f"Score: {self.score}", True, WHITE)
             self.window.blit(score_text, (10, 10))
             pygame.display.flip()
