55b13cd3693d61ced253c89e0fb92b0714374298
DanielKNU <naumenkodaniel003@knu.ua>
2025-03-12 11:02:36 +0200

    add music

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ pygame.display.set_caption("Pac Man: FunModify")
 
 font = pygame.font.Font(None, 36)
 
-background_image = pygame.image.load("background.png").convert()  
+background_image = pygame.image.load("background.png").convert()
 background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+
+pygame.mixer.init()
+pygame.mixer.music.load("menu_song.mp3") 
+pygame.mixer.music.set_volume(0.5)  
+pygame.mixer.music.play(-1) 
 
 def draw_button(text, x, y, width, height, color, text_color):
     pygame.draw.rect(window, color, (x, y, width, height))
     label = font.render(text, True, text_color)
     window.blit(label, (x + (width - label.get_width()) // 2, y + (height - label.get_height()) // 2))
-    return (x, y, width, height)  
+    return (x, y, width, height)
 
 def main_menu():
     running = True
-    start_button_rect = (250, 250, 200, 50)  
+    start_button_rect = (250, 250, 200, 50)
     info_button_rect = (250, 350, 200, 50)
 
     while running:
@@ -31,10 +36,11 @@ def main_menu():
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 x, y, w, h = start_button_rect
                 if x <= mouse_x <= x + w and y <= mouse_y <= y + h:
+                    pygame.mixer.music.stop() 
                     game = Game()
                     game.run()
+                    pygame.mixer.music.play(-1)  
                     return
-                # Проверка клика по кнопке "Info"
                 x, y, w, h = info_button_rect
                 if x <= mouse_x <= x + w and y <= mouse_y <= y + h:
                     show_info()
