c76a91527ed9d2ca587204436e87a0a6027ac31e
nwpivpe <sanyaaaap@gmail.com>
2025-04-13 18:27:17 +0300

    pytestCoin+

---
+++ tests/test_coin.py
@@ -0,0 +1,25 @@
+import pygame
+import pytest
+from entities import Coin
+from settings import *
+
+@pytest.fixture
+def coin():
+    return Coin(50, 50)
+
+def test_coin_initialization(coin):
+    assert coin.rect.x == 50
+    assert coin.rect.y == 50
+    assert coin.rect.width == COIN_SIZE
+    assert coin.rect.height == COIN_SIZE
+    assert coin.collected == False
+
+def test_coin_draw(coin, mocker):
+    window = mocker.Mock(spec=pygame.Surface)
+    mock_draw = mocker.patch("pygame.draw.rect")
+    coin.draw(window)
+    if not coin.collected:
+        mock_draw.assert_called_once_with(window, YELLOW, coin.rect)
+    else:
+        mock_draw.assert_not_called()
+        
